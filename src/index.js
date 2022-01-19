989df85b8c5db4fd46948e6efdcfb11d150eb598
ryong9rrr <tkddbs1644@gmail.com>
2022-01-19 19:29:57 +0900

    feat: 리스트 추가기능, 리스트 count 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,1 +1,71 @@
-console.log("hello world!");
+import { $ } from "./js/DOM.js";
+import { Valid } from "./js/Valid.js";
+
+function App() {
+  const formPreventDefault = () => {
+    return $("#form").addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+  };
+
+  const makeList = (name, mbti) => {
+    return `
+      <li>
+        <div class="mbti-list-name">
+          <span>${name}</span>
+          <button class="btn-edit btn-edit-name shadow">수정</button>
+        </div>
+        <div class="mbti-list-mbti">
+          <span>${mbti}</span>
+          <button class="btn-edit btn-edit-mbti shadow">수정</button>
+        </div>
+        <button class="btn-delete shadow">삭제</button>
+      </li>
+    `;
+  };
+
+  const addList = () => {
+    const name = $("#form-name").value;
+    const mbti = $("#form-mbti").value;
+    const valid = new Valid(name, mbti);
+    const { ok, message } = valid.isValid();
+    if (!ok) return alert(message);
+    const listTemplate = makeList(name, mbti.toUpperCase());
+    $("#form-name").value = $("#form-mbti").value = "";
+    $("#form-name").focus();
+    $(".mbti-list").insertAdjacentHTML("beforeend", listTemplate);
+    const count = $(".mbti-list").querySelectorAll("li").length;
+    $(".mbti-count").textContent = `총 ${count}명`;
+  };
+
+  // 이벤트
+
+  formPreventDefault();
+
+  //추가
+  $(".btn-add").addEventListener("click", addList);
+  $("#form").addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      addList();
+    }
+  });
+
+  //수정, 삭제
+  $(".mbti-list").addEventListener("click", (e) => {
+    // 이름 수정
+    if (e.target.classList.contains("btn-edit-name")) {
+      console.log("이름 수정");
+      return;
+    }
+    // mbti 수정
+    if (e.target.classList.contains("btn-edit-mbti")) {
+      console.log("mbti 수정");
+    }
+    // 삭제
+    if (e.target.classList.contains("btn-delete")) {
+      console.log("리스트 삭제");
+    }
+  });
+}
+
+App();
---
+++ src/js/Valid.js
@@ -0,0 +1,37 @@
+export class Valid {
+  constructor(name, mbti) {
+    this.name = name;
+    this.mbti = mbti;
+  }
+
+  #isValidName = () => {
+    // if 중복된 이름이 있으면 return false;
+    return true;
+  };
+
+  #isValidMbti = () => {
+    if (this.mbti.length !== 4) return false;
+    const mbti = this.mbti.toUpperCase();
+    const [a, b, c, d] = Array.from(mbti);
+    if (
+      !(a === "E" || a == "I") ||
+      !(b === "N" || b == "S") ||
+      !(c === "F" || c == "T") ||
+      !(d === "J" || d == "P")
+    )
+      return false;
+    return true;
+  };
+
+  isValid = () => {
+    if (this.name == "" || this.mbti == "")
+      return { ok: false, message: "이름과 mbti를 모두 입력해주세요." };
+
+    if (!this.#isValidMbti())
+      return { ok: false, message: "올바른 mbti를 입력해주세요." };
+
+    if (!this.#isValidName())
+      return { ok: false, message: "중복된 이름이 있어요." };
+    return { ok: true, message: null };
+  };
+}
